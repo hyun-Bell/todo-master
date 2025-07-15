aa954917aa6ed595bcced4322d79ab0a0b099c6d
hyunBell <man.of.backend@gmail.com>
2025-07-16 02:46:58 +0900

    chore: gitignore에 빌드 파일 및 환경 변수 파일 추가 (#14)

--- backend/dist/prisma/prisma.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    private readonly logger;
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-}
